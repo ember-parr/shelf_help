55042225d0be9c33c8dca366a266e59024b70d4e
Ember Parr <ember.devops@gmail.com>
2021-01-30 16:43:47 -0600

    MealType controller complete

---
+++ Shelf_Help/Controllers/MealTypeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Shelf_Help.Models;
+using Shelf_Help.Repositories;
+
+namespace Shelf_Help.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MealTypeController : ControllerBase
+    {
+        private IMealTypeRepository _mealTypeRepo;
+        private IUserProfileRepository _userRepo;
+
+        public MealTypeController(IMealTypeRepository mealTypeRepo, IUserProfileRepository userRepo)
+        {
+            _mealTypeRepo = mealTypeRepo;
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var allMealTypes = _mealTypeRepo.GetAll();
+            return Ok(allMealTypes);
+        }
+    }
+}
+
+// shouldn't need a post or put methods.... included User repo info but can likely remove later, not currently used. 
