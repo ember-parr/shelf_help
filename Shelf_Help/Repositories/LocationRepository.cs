11f9278410e18c959a77fd21273615516d70ba3e
Ember Parr <ember.devops@gmail.com>
2021-01-29 21:51:35 -0600

    added location repository and interface with location view model

---
+++ Shelf_Help/Repositories/LocationRepository.cs
@@ -0,0 +1,42 @@
+﻿using Shelf_Help.Data;
+using Shelf_Help.Models;
+using Shelf_Help.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shelf_Help.Repositories
+{
+    public class LocationRepository
+    {
+        private ApplicationDbContext _context;
+
+        public LocationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //get a location by id
+        public Location GetById(int id)
+        {
+            return _context.Location
+                .Where(l => l.Id == id)
+                .FirstOrDefault();
+        }
+
+        // get all locations
+        public List<LocationItem> GetAll()
+        {
+            return _context.Location
+                .Select(l => new LocationItem()
+                {
+                    Id = l.Id,
+                    Name = l.Name
+                })
+                .ToList();
+        }
+    }
+}
