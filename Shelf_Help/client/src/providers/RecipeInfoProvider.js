1e4bab3a0306f879d5acb61682e61617b9a16db4
Ember Parr <ember.devops@gmail.com>
2021-02-03 13:11:01 -0600

    fixed recipe info provider

--- Shelf_Help/client/src/providers/RecipeInfoProvider.js
+++ Shelf_Help/client/src/providers/RecipeInfoProvider.js
@@ -1,7 +1,19 @@
 
 
 export const RecipeInfoProvider = (menu) => {
-    const spoonacualrAPIInformation = `https://api.spoonacular.com/recipes/${menu.spoonacularRecipeId}/information`
 
-    return spoonacualrAPIInformation;
+    const recipeId = menu.spoonacularRecipeId;
+    let recipeDetails = {};
+    const getFullRecipe = (recipeId) => {
+        return fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=66e7421be84e4b16a934c4ad2b86bfd4`)
+        .then(response => response.json())
+        .then(parsedRecipe => {
+            recipeDetails = parsedRecipe[0];
+        })
+    };
+
+    getFullRecipe(recipeId);
+    
+
+    return recipeDetails;
 }
